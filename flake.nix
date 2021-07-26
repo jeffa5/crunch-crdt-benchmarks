b79ba9af8b49de51e65563533d616cf41b28892a
Andrew Jeffery <dev@jeffas.io>
2021-07-26 14:20:20 +0100

    Use submodules and add nix config

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "CRDT benchmarks";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs
+            {
+              system = system;
+            };
+        in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs
+
+              python
+
+              rnix-lsp
+              nixpkgs-fmt
+            ];
+          };
+        });
+}
